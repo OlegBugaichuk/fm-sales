ce537a7e5f9737190c81a45c56337f8368fe2d6b
OlegBugaichuk <o.bugaichuk@directline.digital>
2021-03-30 02:01:50 +0400

    add stats and current user

--- src/api/users/routers.py
+++ src/api/users/routers.py
@@ -1,17 +1,42 @@
+import datetime
+from typing import List
 from fastapi import APIRouter, HTTPException, Depends, status
 from core.auth.depends import token_depend
 from core.auth.schemas import Token
-from .schemas import User
-from .crud import get_current_user
+from .schemas import User, UserStat
+from .crud import get_current_user, get_users_list, get_user_by_id, get_current_user_stat
 
 
 router = APIRouter()
 
 
 @router.get('/me', response_model=User)
-async def me(token:Token = Depends(token_depend)) -> User:
+async def me(token:Token = Depends(token_depend)):
     try:
         user = get_current_user(token)
     except Exception:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Check Token')
     return user
+
+
+@router.get('/', response_model=List[User])
+async def get_users(token: Token = Depends(token_depend)):
+    users = get_users_list()
+    return users
+
+
+@router.get('/{user_id}', response_model=User)
+async def get_users(user_id: int, token: Token = Depends(token_depend)):
+    user = get_user_by_id(user_id)
+    if user:
+        return user
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+
+
+@router.get('/me/stats', response_model=UserStat)
+async def get_current_user_stats(token: Token = Depends(token_depend), 
+                  year: int = datetime.datetime.now().year, 
+                  month: int = datetime.datetime.now().month):
+
+    return get_current_user_stat(token, year, month)
--- src/api/users/schemas.py
+++ src/api/users/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-
+from typing import List
 
 class UserBase(BaseModel):
     id: int
@@ -14,3 +14,18 @@ class UserInDb(UserBase):
 
 class User(UserBase):
     pass
+
+
+class UserPlan(BaseModel):
+    level: int
+    value: int
+
+
+class UserStat(BaseModel):
+    total_sales: int = 0
+    bonus: int = 0
+    premium: int = 0
+    premium_percent: float = 0.0
+    salary: int = 0
+    total_salary: int = 0
+    plans: List[UserPlan] = []
