ca1a0efcfa5575f9bae37fe59ad39b1eba8116c1
Oleg Bugaichuk <olegussek@gmail.com>
2021-04-01 17:00:59 +0400

    f

--- src/api/users/crud.py
+++ src/api/users/crud.py
@@ -1,9 +1,10 @@
 from sqlalchemy import text
 from core.auth.schemas import Token
-
+from typing import List
 from core.db.base import engine
 from core.db.tables import tables
 from core.auth.helpers import decode_access_token
+from pydantic import parse_obj_as
 from .schemas import UserInDb, User
 
 def get_user_by_login(login: str) -> UserInDb:
@@ -31,3 +32,14 @@ def get_current_user(token: Token) -> User:
     payload = decode_access_token(token)
     if payload:
         return get_user_by_id(payload.get('user_id', None))
+
+
+def get_users_list() -> List[User]:
+    with engine.connect() as connection:
+        result = connection.execute(text(f'select * from {tables["users"]}'))
+        try:
+            users_list = parse_obj_as(List[User], result.fetchall())
+            return users_list
+        except Exception as e:
+            print(e)
+            return None
--- src/api/users/routers.py
+++ src/api/users/routers.py
@@ -1,17 +1,29 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from core.auth.depends import token_depend
 from core.auth.schemas import Token
+from typing import List
 from .schemas import User
-from .crud import get_current_user
+from .crud import get_current_user, get_users_list
 
 
 router = APIRouter()
 
 
-@router.get('/me')
-async def me(token:Token = Depends(token_depend)) -> User:
+@router.get('/', response_model=List[User])
+async def users_list(token:Token = Depends(token_depend)):
+    try:
+        users = get_users_list()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Check Token')
+    return users
+
+
+@router.get('/me', response_model=User)
+async def me(token:Token = Depends(token_depend)):
     try:
         user = get_current_user(token)
     except Exception:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Check Token')
     return user
+
+
